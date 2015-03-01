0ae957667628f34c64aae43b00c7702e28438d99
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-03-01 19:55:20 -0300

    Add Post Model and Controller

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Controller names are set in plural and always ends
+with Controller.
+ */
+class PostsController extends AppController {
+	/*
+	Helpers are classes used to help writing the views,
+	they can format values or print HTML fragments, for example.
+	 */
+	public $helpers = array('Html', 'Form');
+}
---
+++ app/Model/Post.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Model names are set in singular.
+
+Every Model extends AppModel,
+you can use ir (AppModel) to set defaults,
+like extra validation methods.
+ */
+class Post extends AppModel {
+
+}
