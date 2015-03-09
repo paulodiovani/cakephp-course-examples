985853be1cafabdf5aaeb459374bc8565ec23f58
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-03-08 21:48:48 -0300

    Remove event on CommentsTable
    This event prevents duplicated emails and that is not required
    for comments.

--- src/Model/Table/CommentsTable.php
+++ src/Model/Table/CommentsTable.php
@@ -54,18 +54,4 @@ class CommentsTable extends Table
 
         return $validator;
     }
-
-    /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
-     */
-    public function buildRules(RulesChecker $rules)
-    {
-        $rules->add($rules->isUnique(['email']));
-        $rules->add($rules->existsIn(['post_id'], 'Posts'));
-        return $rules;
-    }
 }
